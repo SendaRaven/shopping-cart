4d367fe5902a86d3801d0b01d234a4be1cfc4bce
SendaRaven <42833205+SendaRaven@users.noreply.github.com>
2019-08-30 13:55:18 +0200

    Setting up the basic file structur and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Shop from './components/Shop';
+import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   return (
     <div className="App">
-
+      <Shop />
+      <ShoppingCart />
     </div>
   );
 }
