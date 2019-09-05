c924b99a9a848b53150fbf4a5165954df5801c56
SendaRaven <r.sahlmann@gmx.de>
2019-09-05 16:02:44 +0200

    Second reducer started

--- src/components/redux/action.js
+++ src/components/redux/action.js
@@ -1,7 +1,7 @@
 import { CHECKOUT, REMOVE_ALL, REMOVE_ONE, ADD_TOCART } from "./action-type";
 
 export function addToCart(payload) {
-    return { type: ADD_TOCART, payload } 
+    return { type: ADD_TOCART, payload }
 };
 export function removeOne(payload) { return { type: REMOVE_ONE, payload } };
 export function removeAll(payload) { return { type: REMOVE_ALL, payload } };
--- src/components/redux/reducer.js
+++ src/components/redux/reducer.js
@@ -7,35 +7,70 @@ const initialState = {
     },
     shoppingCartData: {
         shoppingCartItems: [
-           
+
         ],
     }
 };
 
-export default (state = initialState, { type, payload }) => {
+export default (state = initialState, { type, payload, }) => {
+
+    function updateObjectInShopArray(array, payload) {
+        console.log(payload);
+
+        return array.map((item, index) => {
+            console.log(index, payload.id - 1);
+            console.log(item);
+
+            if (index !== payload.id - 1) {
+                // This isn't the item we care about - keep it as-is
+                return item
+            }
+            // Otherwise, this is the one we want - return an updated value
+            return {
+                ...item,
+                ...item.inventory = item.inventory - 1
+            }
+        })
+    }
+    function updateObjectInCartArray(array, payload) {
+        return (
+            array.map((item, index) => {
+            if (index !== payload.id - 1) {
+                return item
+            }
+        })
+
+        )
+    }
+
     switch (type) {
 
         case ADD_TOCART:
-            
-            return { ...state, 
-                shopData:{
-                shopInventory:[...state.shopData.shopInventory {...state.shopData.shopInventory : state.invetory-1}]
-            },
-            shoppingCartData:{
-                shoppingCartItems:[
-                ...state.shoppingCartData.shoppingCartItems, {
-                    ...payload, count: (state.count===undefined)? 1 : state.count+1
+
+
+
+            return {
+                ...state,
+                shopData: {
+                    shopInventory: updateObjectInShopArray(state.shopData.shopInventory, ...payload)
+                },
+                shoppingCartData: {
+                    shoppingCartItems: updateObjectInCartArray(state.shoppingCartData.shoppingCartItems, payload)
+
+
+
                 }
-                 
-            ]}};
+            };
         case REMOVE_ONE:
             return { ...state, ...payload }
         case REMOVE_ALL:
             return { ...state, ...payload }
         case CHECKOUT:
-            return { ...state, shoppingCartData:{
-                shoppingCartItems:[]
-            } }
+            return {
+                ...state, shoppingCartData: {
+                    shoppingCartItems: []
+                }
+            }
 
         default:
             return state
